20736e53bb4432251bfe038acf5a8774b650b3dc
Altair Martinez <altair.martinez@eonreality.com>
2018-04-23 16:58:05 +0800

    Added language

--- Tables/dbo.Horoscope.sql
+++ Tables/dbo.Horoscope.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[Horoscope]
 (
-[daily] [nvarchar] (1024) COLLATE Latin1_General_CI_AI NOT NULL
+[daily] [nvarchar] (1024) COLLATE Latin1_General_CI_AI NOT NULL,
+[language] [nvarchar] (8) COLLATE Latin1_General_CI_AI NULL
 ) ON [PRIMARY]
 GO
