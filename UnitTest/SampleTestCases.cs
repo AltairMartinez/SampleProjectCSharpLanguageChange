1c207c3406958eb77dba737e4503d17a6decaf93
Altair Martinez <altair.martinez@eonreality.com>
2018-05-04 15:25:46 +0800

    +add a DEV test and SMOKETEST

--- UnitTest/SampleTestCases.cs
+++ UnitTest/SampleTestCases.cs
@@ -7,7 +7,7 @@ using System.Web;
 namespace LocalizationTest.UnitTest
 {
     [TestFixture]
-    public class SampleTestCases
+    public class SampleTestCases_DEV
     {
         [TestCase]
         public void TestOne()
@@ -28,6 +28,35 @@ namespace LocalizationTest.UnitTest
 
         [TestCase]
         public void TestThree()
+        {
+            TestableClass.currentLanguage = "";// "CHINESE";
+            Assert.AreEqual("", TestableClass.currentLanguage);
+        }
+
+    }
+
+    [TestFixture]
+    public class SampleTestCases_SMOKE
+    {
+        [TestCase]
+        public void TestOneSmoke()
+        {
+            //TestableClass testSample = new TestableClass();
+            //testSample.m_currentLanguage = "zh";
+
+            TestableClass.currentLanguage = "zh";
+            Assert.AreEqual("zh", TestableClass.currentLanguage);
+        }
+
+        [TestCase]
+        public void TestTwoSmoke()
+        {
+            TestableClass.currentLanguage = "jp";// "CHINESE";
+            Assert.AreEqual("jp", TestableClass.currentLanguage);
+        }
+
+        [TestCase]
+        public void TestThreeSmoke()
         {
             TestableClass.currentLanguage = "";// "CHINESE";
             Assert.AreEqual("", TestableClass.currentLanguage);
